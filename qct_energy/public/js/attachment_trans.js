9637b21fccc13abc949b52583aee7209cb7bb627
suraj <suraj@techlift.in>
2019-11-06 18:44:48 +0530

    attachment transfer to quotation

---
+++ qct_energy/public/js/attachment_trans.js
@@ -0,0 +1,18 @@
+frappe.ui.form.on("Quotation",{
+refresh: function(frm) {
+    frm.add_custom_button(__("Load Attachments"), function(foo) {
+
+	frappe.call({
+		method:"qct_energy.item_attachment.attach_trans.attach_all_docs",
+		args: {
+			document: cur_frm.doc,
+			
+		}, 
+		callback: function(r) { 
+			frm.reload_doc();
+
+		}
+	});
+    });
+    }
+});
