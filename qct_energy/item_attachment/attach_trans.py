ab535d072035154bc674f45344c6bd937a337090
Your Name <varade.suraj787@gmail.com>
2019-11-14 22:59:55 +0530

    zone doctype removed

--- qct_energy/item_attachment/attach_trans.py
+++ qct_energy/item_attachment/attach_trans.py
@@ -3,41 +3,33 @@ import json
 
 @frappe.whitelist()
 def attach_all_docs(document, method=None):
+	"""This function attaches drawings to the purchase order based on the items being ordered"""
 	document = json.loads(document)
-	current_attachments=[]
-	print("@@@@@@@@@@@@_---------------------")
-	a=("""select file_url from `tabFile` where attached_to_doctype = "Item" and attached_to_name = 'test' """)
-	print(a)
-	b=frappe.db.sql(a, as_dict=True)
-	for file_url in b :
+
+	current_attachments = []
+	for file_url in frappe.db.sql("""select file_url from `tabFile` where attached_to_doctype = %(doctype)s and     attached_to_name = %(docname)s""", {'doctype': document["doctype"], 'docname': document["name"]}, as_dict=True ):
 		current_attachments.append(file_url.file_url)
+		print(file_url)
 		count = 0
 		for item_doc in document["items"]:
-			print("$$$$$$$$$$$$$$$$$$$")
-			print(item_doc)
-			#frappe.msgprint(item_doc)
+			rappe.msgprint(item_doc)
 			# Check to see if the quantity is = 1
-			print("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
-			print(item_doc["item_code"])
-			if item_doc["item_code"]:
-				item = frappe.get_doc("Item",item_doc["item_code"])
-				print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-				print(item)
+			item = frappe.get_doc("Item",item_doc["item_code"])
 	
-				attachments = []
-				# Get the path for the attachments
-				if item.drawing_attachment:
-					attachments.append(item.drawing_attachment)
-				if item.stp_attachment:
-					attachments.append(item.stp_attachment)
-				if item.dxf_attachment:
-					attachments.append(item.dxf_attachment)
-				if item.x_t_attachment:
-					attachments.append(item.x_t_attachment)
-			
-				for attach in attachments:
-					# Check to see if this file is attached to the one we are looking for
-					if not attach in current_attachments:
-						count = count + 1
-						save_url(attach, document["doctype"], document["name"], "Home/Attachments")
-						frappe.msgprint("Attached {0} files".format(count))
+			attachments = []
+			# Get the path for the attachments
+			if item.drawing_attachment:
+				attachments.append(item.drawing_attachment)
+			# if item.stp_attachment:
+			#     attachments.append(item.stp_attachment)
+			# if item.dxf_attachment:
+			#     attachments.append(item.dxf_attachment)
+			# if item.x_t_attachment:
+			#     attachments.append(item.x_t_attachment)
+		
+			for attach in attachments:
+				# Check to see if this file is attached to the one we are looking for
+				if not attach in current_attachments:
+					count = count + 1
+					save_url(attach, document["doctype"], document["name"], "Home/Attachments")
+			frappe.msgprint("Attached {0} files".format(count))
